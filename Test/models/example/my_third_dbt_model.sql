431c71809b00136923e6796a46bd6dde9dff1ea4
OmoIke-cyber <omo.ike@halomd.com>
2025-10-22 06:11:23 -0400

    Updated model naming convention

--- Test/models/example/my_third_dbt_model.sql
+++ Test/models/example/my_third_dbt_model.sql
@@ -3,4 +3,4 @@ select * from  {{ ref('my_first_dbt_model') }}
 union all
 
 select ARBITRATIONCASEID, 'NX' as location, 'Debby' as manager
-from {{ source('test_source','DISPUTE_SETTLEMENT')}}
+from {{ ref('stg_core_disputes__DISPUTE__SETTLEMENT')}}
